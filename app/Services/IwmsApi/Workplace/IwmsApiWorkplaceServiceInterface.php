32297f4b199486de2ef3ea140e0abe48b20337ad
@arman <arman.arakelyan@develit.se>
2022-10-11 13:15:15 +0400

    Revert "Create a console command to sync companies and contractors from the IWMS"
    This reverts commit ac9a2a30424f2cb53d53dc1f8db8eeaf567bc13f.

--- app/Services/IwmsApi/Workplace/IwmsApiWorkplaceServiceInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services\IwmsApi\Workplace;
-
-interface IwmsApiWorkplaceServiceInterface
-{
-    public function workplace(): IwmsAPIPaginationResponse;
-}
--- config/iwms.php
+++ config/iwms.php
@@ -2,5 +2,4 @@
 
 return [
     'api_base_url' => env('IWMS_API_BASE_URL', ''),
-    'api_user_token' => env('IWMS_API_USER_TOKEN', '')
 ];
