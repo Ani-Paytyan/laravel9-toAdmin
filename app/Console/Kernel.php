a887b3f396c9717bdd5b7ce85923301d42edf7a4
I. Kvilinskiy <ihor.kvilinskiy@gmail.com>
2022-10-18 09:19:18 +0300

    Commands scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CompaniesFetch;
+use App\Console\Commands\WorkplacesFetch;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +17,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command(CompaniesFetch::class)->everyFifteenMinutes();
+        $schedule->command(WorkplacesFetch::class)->everyFifteenMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
