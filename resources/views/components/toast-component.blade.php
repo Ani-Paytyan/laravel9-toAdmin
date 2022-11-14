4a74583efa5d05832e4a7a8e0fc726bea3ed499f
@arman <arman.arakelyan@develit.se>
2022-11-14 13:19:27 +0400

    remove id="liveToast" from toast-component

--- resources/views/components/toast-component.blade.php
+++ resources/views/components/toast-component.blade.php
@@ -1,6 +1,6 @@
 @if(session('toast_error') !== null)
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-        <div id="liveToast" class="toast fade show  border-1 border-danger" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast fade show  border-1 border-danger" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <strong class="me-auto text-danger">IWMS API Error</strong>
                 <small>11 mins ago</small>
