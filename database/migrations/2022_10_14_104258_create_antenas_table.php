ad4164b23d002fe443087ce61b0616559b3fd26f
I. Kvilinskiy <ihor.kvilinskiy@gmail.com>
2022-12-08 11:22:00 +0200

    Migration fix

--- database/migrations/2022_10_14_104258_create_antenas_table.php
+++ database/migrations/2022_10_14_104258_create_antenas_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
                 ->references('id')
                 ->on('manufacture_types')
                 ->onUpdate('cascade');
-            $table->boolean('is_online')->nullable();
 
             $table->timestamps();
             $table->softDeletes();
